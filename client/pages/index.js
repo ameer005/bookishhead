a579fcdf65f1e536a1c0c0b65cace406700a3603
Ameer <ak2260650@gmail.com>
2022-09-15 14:55:25 +0530

    added user routes

--- client/pages/index.js
+++
@@ -1,5 +0,0 @@
-const HomePage = () => {
-  return <div>yo home page</div>;
-};
-
-export default HomePage;
--- server/db/connect.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-
-module.exports = (url) =>
-  mongoose.connect(url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
